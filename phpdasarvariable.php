86fbf980f17c043acfba77e411b153bf0fbab958
vlnt <michaellaisina47@gmail.com>
2022-11-28 02:17:09 +0700

    menambahkan file

---
+++ phpdasarvariable.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>PHP Dasar</title>
+  </head>
+  <body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+      echo "Hello World";?>
+      <h1>Menggunakan Variable</h1>
+      <?php
+    $nim = "0411500400";
+    $nama = 'Abdullah';
+    echo "NIM : " . $nim . "<br>"; echo "Nama : $nama";
+?>
+  </body>
+</html>
